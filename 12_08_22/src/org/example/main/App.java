a77eee8daf037b1b8887d61b902066488da66c3d
Nilay <54245048+dnilay@users.noreply.github.com>
2022-08-12 14:13:05 +0530

    Add files via upload

--- 12_08_22/src/org/example/main/App.java
+++ 12_08_22/src/org/example/main/App.java
@@ -1,18 +1,36 @@
 package org.example.main;
 
-import java.util.Random;
-
 import org.example.model.CD;
 import org.example.model.Employee;
 import org.example.model.InternationalCD;
 import org.example.model.SpecialEditionCD;
 
 public class App {
+	
+	
+	public static void approvedLeave(Employee employee)
+	{
+		
+	}
+	public static void checkObject(CD cd)
+	{
+		CD tempCD=null;
+		if(cd instanceof SpecialEditionCD)
+		{
+			tempCD=(SpecialEditionCD)cd;
+			System.out.println("special edition cd:"+tempCD);
+		}
+		else if(cd instanceof InternationalCD)
+		{
+			tempCD=(InternationalCD)cd;
+			System.out.println("international cd: "+tempCD);
+		}
+	}
 
 	public static void main(String[] args) {
 
 		// dynamic method dispatching
-		
+
 		/*
 		 * CD cd=new InternationalCD("French", 100, "Godzila", 10000);
 		 * System.out.println(cd+"\n==============\n"); cd=new SpecialEditionCD(101,
@@ -20,13 +38,21 @@ public class App {
 		 * 
 		 * System.out.println(cd);
 		 */
-		 
-		Employee employee=new Employee(1, "John Doe", 200000);
-		Employee employee1=new Employee(1, "John Doe", 200000);
-		System.out.println(employee==employee1);
-	System.out.println(employee);
-	System.out.println(employee1);
-		System.out.println(employee.equals(employee1));
+
+		/*
+		 * Employee employee = new Employee(1, "John Doe", 200000); Employee employee1 =
+		 * new Employee(1, "John Doe", 200000); System.out.println(employee ==
+		 * employee1); System.out.println(employee); System.out.println(employee1);
+		 * System.out.println(employee.equals(employee1));
+		 */
+		
+		
+		
+		CD cd1=new InternationalCD("French", 1, "mycd-1", 1000);
+		CD cd2=new SpecialEditionCD(2,"mycd-2",200,"3D");
+		checkObject(cd2);
+		
+		
 
 	}
 
---
+++ 12_08_22/src/org/example/model/RegularEmployee.java
@@ -0,0 +1,5 @@
+package org.example.model;
+
+public class RegularEmployee extends Employee{
+
+}
